03252d1f272b7f68e73b646ed039a6b721aae7a0
abolfazl2643 <abolfazlrezaei.v@gmail.com>
2021-10-11 01:05:08 +0330

    create dbcontext interface in application layer

--- Domain/Entities/Advertisement.cs
+++ Domain/Entities/Advertisement.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Domain.Entities
 {
-    class Advertisement
+    public class Advertisement
     {
         public int Id { get; set; }
         public string Title { get; set; }
---
+++ application/Interfaces/Data/IalamutDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application.Interfaces.Data
+{
+    class IAlamutDbContext
+    {
+        public DbSet<Advertisement> Advertisements { get; set; }
+    }
+}
