3505f41c7cc03f68fedc94351c5707bb22175b61
abolfazl2643 <abolfazlrezaei.v@gmail.com>
2021-10-11 00:46:44 +0330

    add di extention methods to app/inf classes

---
+++ Infrastructure/injection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public static class Injection
+    {
+        public static IServiceCollection RegisterInfrastructerServices(
+          this IServiceCollection service,
+          IConfiguration configuration)
+        {
+            return service;
+        }
+    }
+}
---
+++ application/injection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application
+{
+    public static class Injection
+    {
+        public static IServiceCollection RegisterApplicationServices(
+             this IServiceCollection service,
+             IConfiguration configuration)
+        {
+            return service;
+        }
+    }
+}
