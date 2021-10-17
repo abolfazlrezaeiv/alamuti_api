d546f144989805482d5708b9a0f84fd247edd031
abolfazl2643 <abolfazlrezaei.v@gmail.com>
2021-10-17 13:49:14 +0330

    add dto class

---
+++ application/DTOs/AlamutDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application.DTOs
+{
+    class AlamutDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public byte[] Photo { get; set; }
+        public DateTime DatePosted { get; set; }
+    }
+}
